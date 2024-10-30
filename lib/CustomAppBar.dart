f566ca9343c05f24cf49958bd1b5fae5b40d41d6
Crystian <crystiankenji04@gmail.com>
2024-10-30 01:14:15 -0300

    CustomAppBar criado para reutilizar o appBar

---
+++ lib/CustomAppBar.dart
@@ -0,0 +1,36 @@
+// CustomAppBar.dart
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  @override
+  final Size preferredSize; // Define o tamanho do AppBar
+
+  CustomAppBar({Key? key})
+      : preferredSize = const Size.fromHeight(56.0),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: const Color(0xFF0D2838),
+      title: Image.asset(
+        'assets/queijos-finos.png',
+        height: 40,
+      ),
+      iconTheme: const IconThemeData(
+        color: Colors.white,
+      ),
+      actions: [
+        Padding(
+          padding: const EdgeInsets.only(right: 50.0),
+          child: IconButton(
+            icon: const Icon(Icons.notifications),
+            onPressed: () {
+              // Defina uma ação se necessário
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
